95140846775d9aafe2c476ae26166a2a8b0c1fe7
Ankit_S <112768684+Ankitsreenivasa@users.noreply.github.com>
2023-01-26 19:37:30 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -10,7 +10,7 @@ class fibonacci {
  
     public static void main(String args[])
     {
-        int n = 10;
+        int n = 10;//n ia any natural no.
         System.out.println(fib(n));
     }
 }
